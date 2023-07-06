63e0cc3dd2408aa05d8bb412d0ab665e3bba9bb7
agrandesr <agrandesr@gmail.com>
2023-07-06 17:28:19 +0200

    New trustchain query

--- private/Queries.php
+++ private/Queries.php
@@ -1,18 +1,21 @@
 <?php
 namespace Private;
 
+use Exception;
 use Private\Utils\DatabaseTool;
 
 class Queries {
-    static function getTrustChain($id){
+    static function getTrustChain($id, $setter){
+        if(!in_array($setter,['city','land','state'])) throw new Exception("Avoid SQL injection Error", 1);
+        $setter=$setter.'_trust';
         $sql="WITH RECURSIVE recursive_loop  AS (
-            SELECT city_trust FROM newdeal.users WHERE id=:id AND city_trust IS NOT NULL
+            SELECT $setter FROM newdeal.User WHERE id=:id AND $setter IS NOT NULL
             UNION ALL
-            SELECT c.city_trust FROM newdeal.users c
-            INNER JOIN recursive_loop  l ON l.city_trust = c.id
+            SELECT c.$setter FROM newdeal.User c
+            INNER JOIN recursive_loop  l ON l.$setter = c.id
         ) SELECT * FROM recursive_loop ;";
         return array_column(DatabaseTool::sql('',$sql, [
             'id'=>$id
-        ]),'city_trust');
+        ]), $setter);
     }
 }
