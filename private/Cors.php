f7e816c54da492e1ff04d45c7db24a1da7ed6679
agrandesr <agrandesr@gmail.com>
2023-09-11 11:41:32 +0200

    SAve new Cors

--- private/Cors.php
+++ private/Cors.php
@@ -2,17 +2,15 @@
 namespace Private;
 
 class Cors {
-    static function allow() {
+    static function allow(array $url=['http://localhost:3000'], array $methods=[]) {
         $origin = $_SERVER['HTTP_ORIGIN'];
 
-        $allowedOrigins = array(
-            'http://localhost:3000'
-        );
+        $allowedOrigins = $url;
 
         if (in_array($origin, $allowedOrigins)) {
             header('Access-Control-Allow-Origin: ' . $origin);
 
-            header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+            header('Access-Control-Allow-Methods: ' . empty($methods)?'*' : implode(', ', $methods));
             header('Access-Control-Allow-Headers: Authorization, Content-Type');
 
             header('Access-Control-Allow-Credentials: true');
